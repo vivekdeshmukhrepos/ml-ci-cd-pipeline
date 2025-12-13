affdbc1e5206fa0b360184bcbda9a2f88e2a560f
vivekdeshmukhrepos <vivek.deshmukh@live.com>
2025-12-13 10:50:20 +0530

    Created pkl file with the help of transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from  dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 # Create a data ingestion config class
 # If we decorate as @dataclass then we don't need to write init method
 # you can directly define variables inside the class
@@ -54,4 +57,7 @@ class DataIngestion:
         
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
