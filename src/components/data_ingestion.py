97b1f8c32471a4c3b8da4341899dc18bbe945978
vivekdeshmukhrepos <vivek.deshmukh@live.com>
2025-12-20 12:34:29 +0530

    Created pipeline and Flasp app setup

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,6 +1,7 @@
 # Get the data from external sources
 import os
 import sys
+from src.components.model_trainer import ModelTrainer
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
@@ -60,4 +61,7 @@ if __name__ == "__main__":
     train_data, test_data = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr, _ =  data_transformation.initiate_data_transformation(train_data, test_data)
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
+    
